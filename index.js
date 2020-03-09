c59a73823387953b45cb54cad3773eed0ff64600
alexandre jamarco <alexandre.j@gmail.com>
2020-03-09 12:10:53 +0000

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,21 @@
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
+  console.log(timeString);
   displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(str){
+  str = str.split(":").join("");
+  if (str < 1200) return "Good Morning";
+  if (str > 1200 && str < 1700) return "Good Afternoon";
+  return "Good Evening";
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg;
+}
